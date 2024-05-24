881474e5cafc6be3f843088e687aac34dd767ac0
lodge <jay.logelin@gmail.com>
2024-05-24 14:21:44 -0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,27 +13,28 @@ if [ ! -d $LILYPAD_DIR ]; then
     # install lilypad
     echo "Installing Lilypad"
 
-    mkdir $LILYPAD_DIR/code
-    cd $LILYPAD_DIR/code
+    #mkdir $LILYPAD_DIR/code
+    #cd $LILYPAD_DIR/code
 
-    git clone https://github.com/Lilypad-Tech/lilypad.git
-    cd lilypad
-    make
-    cd ../../..
-    mv $LILYPAD_DIR/code/lilypad/bin/lilypad $LILYPAD_DIR
-    rm -rf $LILYPAD_DIR/code
+    #git clone https://github.com/Lilypad-Tech/lilypad.git
+    #cd lilypad
+    #make
+    #cd ../../..
+    #chmod +x $LILYPAD_DIR/code/lilypad/bin/lilypad
+    #mv $LILYPAD_DIR/code/lilypad/bin/lilypad /usr/local/bin
+    #rm -rf $LILYPAD_DIR/code
 
-    #LILYPAD_VERSION="v2.0.0-7b61040"
+    LILYPAD_VERSION="v2.0.0-f957f9e"
     # LILYPAD_BINARY="lilypad-darwin-arm64"
-    #LILYPAD_BINARY="lilypad-linux-amd64"
+    LILYPAD_BINARY="lilypad-linux-amd64"
 
     #sudo apt update
     #sudo apt install libc6 -y
-    #wget https://github.com/Lilypad-Tech/lilypad/releases/download/$LILYPAD_VERSION/$LILYPAD_BINARY
-    #tar xfv $LILYPAD_BINARY
-    #mv $LILYPAD_BINARY $LILYPAD_DIR/lilypad
-    #chmod +x $LILYPAD_DIR/lilypad
-    #rm $LILYPAD_BINARY
+    wget https://github.com/Lilypad-Tech/lilypad/releases/download/$LILYPAD_VERSION/$LILYPAD_BINARY
+    tar xfv $LILYPAD_BINARY
+    mv $LILYPAD_BINARY $LILYPAD_DIR/lilypad
+    chmod +x $LILYPAD_DIR/lilypad
+    rm $LILYPAD_BINARY
 
     echo "Installing Bacalhau"
     # BACALHAU_BINARY="bacalhau_v1.0.3_darwin_arm64.tar.gz"
